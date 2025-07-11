4db0a366b5d11e2a370127ec78489bd1d571f9aa
ZeroK524 <tmnham.thpt.pvd2004@gmail.com>
2025-07-11 22:57:04 +0700

    Encryption

---
+++ Encryption/Encryption/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Encryption
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+			this.label1 = new System.Windows.Forms.Label();
+			this.rtb1 = new System.Windows.Forms.RichTextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.rbtn1 = new System.Windows.Forms.RadioButton();
+			this.rbtn2 = new System.Windows.Forms.RadioButton();
+			this.button1 = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.col1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.col2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(12, 57);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(38, 16);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Input:";
+			// 
+			// rtb1
+			// 
+			this.rtb1.Location = new System.Drawing.Point(56, 39);
+			this.rtb1.Name = "rtb1";
+			this.rtb1.Size = new System.Drawing.Size(509, 57);
+			this.rtb1.TabIndex = 1;
+			this.rtb1.Text = "";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(2, 116);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(48, 16);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Output:";
+			// 
+			// rbtn1
+			// 
+			this.rbtn1.AutoSize = true;
+			this.rbtn1.Location = new System.Drawing.Point(571, 66);
+			this.rbtn1.Name = "rbtn1";
+			this.rbtn1.Size = new System.Drawing.Size(75, 17);
+			this.rbtn1.TabIndex = 7;
+			this.rbtn1.TabStop = true;
+			this.rbtn1.Text = "Encryption";
+			this.rbtn1.UseVisualStyleBackColor = true;
+			// 
+			// rbtn2
+			// 
+			this.rbtn2.AutoSize = true;
+			this.rbtn2.Location = new System.Drawing.Point(571, 43);
+			this.rbtn2.Name = "rbtn2";
+			this.rbtn2.Size = new System.Drawing.Size(63, 17);
+			this.rbtn2.TabIndex = 8;
+			this.rbtn2.TabStop = true;
+			this.rbtn2.Text = "Decode";
+			this.rbtn2.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(672, 43);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(96, 46);
+			this.button1.TabIndex = 9;
+			this.button1.Text = "Run";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+			dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+			dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+			dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+			dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+			dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+			this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.col1,
+            this.col2});
+			this.dataGridView1.Location = new System.Drawing.Point(56, 116);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersVisible = false;
+			this.dataGridView1.ShowCellErrors = false;
+			this.dataGridView1.Size = new System.Drawing.Size(732, 322);
+			this.dataGridView1.TabIndex = 10;
+			
+			// 
+			// col1
+			// 
+			this.col1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+			this.col1.FillWeight = 20F;
+			this.col1.HeaderText = "Encoding Method";
+			this.col1.Name = "col1";
+			// 
+			// col2
+			// 
+			this.col2.HeaderText = "Result";
+			this.col2.Name = "col2";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.rbtn2);
+			this.Controls.Add(this.rbtn1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.rtb1);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.RichTextBox rtb1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.RadioButton rbtn1;
+		private System.Windows.Forms.RadioButton rbtn2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn col1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn col2;
+	}
+}
+
---
+++ Encryption/Encryption/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encryption
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			// Đảm bảo có đúng 2 cột
+			dataGridView1.ColumnCount = 2;
+			dataGridView1.Columns[0].Name = "col1";
+			dataGridView1.Columns[1].Name = "col2";
+
+			// Thêm các dòng
+			dataGridView1.Rows.Add("pass", "12345");
+			dataGridView1.Rows.Add("base64", "67891");
+			dataGridView1.Rows.Add("RSA", "abcsd");
+			dataGridView1.Rows.Add("Hoho", "hahha");
+			// Tự động giãn ra cho vừa DataGridView
+			dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+			dataGridView1.Columns[0].FillWeight = 10;  // Cột 1
+			dataGridView1.Columns[1].FillWeight = 90;  // Cột 2
+		}
+
+	}
+}
